b09ce564a59855c29a9287ecff2af9e276d3d79e
Dandywahyudin <dandywahyudin19@gmail.com>
2025-10-30 20:35:12 +0700

    update package

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 import { getAccessToken, getRefreshToken, setAccessToken, clearTokens } from './storage'
 
-const API_BASE_URL = 'http://localhost:3000/api'
+const API_BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:3000/api'
 
 // Create axios instance with default config
 const api = axios.create({
